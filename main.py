1489d79ddf1e50eab98c935a8655a787b896e5ae
bruno-edo <bruno.eduardo.do@gmail.com>
2017-07-05 23:16:57 -0300

    Debugged skeleton a bit

--- main.py
+++ main.py
@@ -1,43 +1,45 @@
 from flask import Flask, request, Blueprint
 
-users_bp= Blueprint('users_blueprint', __name__)
-
+users_bp= Blueprint('users_blueprint', __name__, url_prefix='/users/<string:user_id>')
 app = Flask(__name__)
-app.register_blueprint(users_bp, url_prefix='/users/<string:userid>')
 
-
-@app.route('/urls/<int:id>', methods=['GET', 'DELETE'])
+@app.route('/urls/<int:id>/', methods=['GET', 'DELETE'])
 def urls(id):
     if request.method == 'GET':
-        pass
+        return 'get urls'
     else:
         pass
 
-@users_bp.route('/urls', methods=['POST'])
-def urls(userid):
+@users_bp.route('/urls/', methods=['POST'])
+def urls(user_id):
     pass
 
-@users_bp.route('/stats', methods=['GET'])
-def stats(userid):
-    pass
+@users_bp.route('/stats/', methods=['GET'])
+def stats(user_id):
+    return 'user stats'
 
-@app.route('/stats', methods=['GET']) #Returns global stats
-@app.route('/stats/<int:id>', methods=['GET']) #Returns stats from a specific URL
-def stats(): #global
-    pass
-def stats(id): #specific url
-    pass
+@app.route('/stats/', methods=['GET']) #Returns global stats
+@app.route('/stats/<int:stats_id>/', methods=['GET']) #Returns stats from a specific URL
+def global_stats(stats_id=None):
+    if stats_id is None: #global
+        return 'global stats'
 
-@app.route('/users', methods=['POST'])
+    else: #specific
+        return 'specific stats'
+
+
+@app.route('/users/', methods=['POST'])
 def users():
     json_data = request.get_json()
     user_id = json_data['id']
     print(json_data)
 
-@app.route('/user/<string:userid>', methods=['DELETE']) #DELETE /user/:userId was the specification. Maybe they meant to utilize 'users' like in the other URLs?
-def delete(userid):
+@app.route('/user/<string:user_id>/', methods=['DELETE']) #DELETE /user/:userId was the specification. Maybe they meant to utilize 'users' like in the other URLs?
+def delete(user_id):
     pass
 
 if __name__ == '__main__':
-    pass
+    app.register_blueprint(users_bp)
+    app.config['DEBUG'] = True
+    app.run(host='localhost', port=5000)
     #ContentType: application/json -> para todos os métodos menos o de redirecão
