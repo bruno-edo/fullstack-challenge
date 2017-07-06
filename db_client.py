e94e2a7e44ab3b2e6324826e4f1f2775c9e2f092
bruno-edo <bruno.eduardo.do@gmail.com>
2017-07-06 00:55:21 -0300

    Added TinyDB to the project and finished the add users method

---
+++ db_client.py
@@ -0,0 +1,25 @@
+from tinydb import TinyDB, Query
+
+db = TinyDB('database/db.json')
+users_table = db.table(name='Users')
+urls_table = db.table(name='URLs')
+
+def create_new_url():
+    pass
+
+def create_new_user(user_id):
+    User = Query()
+    query_response = users_table.search(User.user_id == user_id)
+
+    if len(query_response) > 0:
+        return False
+
+    else:
+        users_table.insert(
+            {
+                'user_id' : user_id.lower(),
+                'hits' : 0,
+                'url_count' : 0
+            }
+        )
+        return True
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-from flask import Flask, request, Blueprint
+import db_client
 
-users_bp= Blueprint('users_blueprint', __name__, url_prefix='/users/<string:user_id>')
+from flask import Flask, request, Blueprint, Response, jsonify
+
+users_bp = Blueprint('users_blueprint', __name__, url_prefix='/users/<string:user_id>')
+stats_bp = Blueprint('stats_blueprint', __name__, url_prefix='/stats')
 app = Flask(__name__)
 
-@app.route('/urls/<int:id>/', methods=['GET', 'DELETE'])
+@app.route('/urls/<int:id>', methods=['GET', 'DELETE'], strict_slashes=False)
 def urls(id):
     if request.method == 'GET':
         return 'get urls'
     else:
         pass
 
-@users_bp.route('/urls/', methods=['POST'])
+@users_bp.route('/urls', methods=['POST'], strict_slashes=False)
 def urls(user_id):
     pass
 
-@users_bp.route('/stats/', methods=['GET'])
+@users_bp.route('/stats', methods=['GET'], strict_slashes=False)
 def stats(user_id):
     return 'user stats'
 
-@app.route('/stats/', methods=['GET']) #Returns global stats
-@app.route('/stats/<int:stats_id>/', methods=['GET']) #Returns stats from a specific URL
+@stats_bp.route('/', methods=['GET']) #Returns global stats
+@stats_bp.route('/<int:stats_id>', methods=['GET'], strict_slashes=False) #Returns stats from a specific URL
 def global_stats(stats_id=None):
     if stats_id is None: #global
         return 'global stats'
@@ -27,19 +30,31 @@ def global_stats(stats_id=None):
     else: #specific
         return 'specific stats'
 
-
-@app.route('/users/', methods=['POST'])
+@app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
-    json_data = request.get_json()
+    json_data = request.get_json(force=True)
     user_id = json_data['id']
     print(json_data)
 
-@app.route('/user/<string:user_id>/', methods=['DELETE']) #DELETE /user/:userId was the specification. Maybe they meant to utilize 'users' like in the other URLs?
+    db_response = db_client.create_new_user(user_id)
+
+    if db_response:
+        response = Response(response=jsonify({'id' : user_id}), status=201, mimetype='application/json')
+
+    else: #error: already exists
+        response = Response(status=409)
+
+    return response
+
+@app.route('/user/<string:user_id>', methods=['DELETE'], strict_slashes=False) #DELETE /user/:userId was the specification. Maybe they meant to utilize 'users' like in the other URLs?
 def delete(user_id):
     pass
 
+#These must be placed here, as the code that runs the blueprints has to be instantiated before registering the blueprint
+app.register_blueprint(users_bp)
+app.register_blueprint(stats_bp)
+
 if __name__ == '__main__':
-    app.register_blueprint(users_bp)
     app.config['DEBUG'] = True
     app.run(host='localhost', port=5000)
     #ContentType: application/json -> para todos os métodos menos o de redirecão
