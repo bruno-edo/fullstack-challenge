a5225bb60abd31455e765f80e1170cad65ba8276
bruno-edo <bruno.eduardo.do@gmail.com>
2017-07-07 02:56:48 -0300

    Worked on all the methods, still a few TODOs pending

--- db_client.py
+++ db_client.py
@@ -26,11 +26,60 @@ cursor.execute("""
     )
 """)
 
-# Save (commit) the changes
 conn.commit()
 conn.close()
 
-def get_user_stats(user_id): #TODO: finish this method by adding the top urls by user
+def get_url(url_id):
+    conn = sqlite3.connect('database/challenge.db')
+    conn.execute('pragma foreign_keys=ON') #Turns on foreign key constraints
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT url FROM URL WHERE id = ?
+    """, (url_id, ))
+
+    data = cursor.fetchone()
+
+    cursor.execute("""UPDATE URL SET hits=hits+1 WHERE id=?
+    """, (url_id, ))
+
+    conn.commit()
+    conn.close()
+
+    if data is None:
+        return False, None
+
+    else:
+        return True, data[0]
+
+def get_top_urls(user_id=None):
+    conn = sqlite3.connect('database/challenge.db')
+    cursor = conn.cursor()
+    if user_id is None:
+        cursor.execute("""
+        SELECT * FROM URL ORDER BY hits DESC LIMIT 10
+        """)
+    else:
+        cursor.execute("""
+        SELECT * FROM URL WHERE userId=? ORDER BY hits DESC LIMIT 10
+        """, (user_id, ))
+
+    data = cursor.fetchall()
+    conn.close()
+
+    url_list = []
+    for row in data:
+        stats = {
+            'id': row[0],
+            'hits': row[1],
+            'url': row[2],
+            'shortUrl': row[3]
+        }
+        url_list.append(stats)
+
+    return url_list
+
+def get_user_stats(user_id): #TODO: test if top urls by user are right
     conn = sqlite3.connect('database/challenge.db')
     conn.execute('pragma foreign_keys=ON') #Turns on foreign key constraints
     cursor = conn.cursor()
@@ -49,17 +98,17 @@ def get_user_stats(user_id): #TODO: finish this method by adding the top urls by
             'id': data[0],
             'hits': data[1],
             'urlCount': data[2]
+            'topUrls': get_top_urls(user_id)
         }
         return True, stats
 
 def get_url_stats(url_id):
     conn = sqlite3.connect('database/challenge.db')
-    conn.execute('pragma foreign_keys=ON') #Turns on foreign key constraints
     cursor = conn.cursor()
 
     cursor.execute("""
         SELECT * FROM URL WHERE id = ?
-    """, (url_id,))
+    """, (url_id, ))
     data = cursor.fetchone()
     conn.close()
 
@@ -75,7 +124,30 @@ def get_url_stats(url_id):
         }
         return True, stats
 
-def create_new_url(url, partial_short_url, user_id):
+def get_global_stats():
+    conn = sqlite3.connect('database/challenge.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT count(), sum(hits) FROM URL
+    """)
+    data = cursor.fetchone()
+    conn.commit()
+    conn.close()
+
+    url_count = data[0]
+    hits_sum = data[1]
+    top_urls = get_top_urls()
+
+    response = {
+        'hits': hits_sum,
+        'urlCount': url_count,
+        'topUrls': top_urls
+    }
+
+    return response
+
+def create_new_url(url, partial_short_url, user_id): #TODO: incremment user url count
     conn = sqlite3.connect('database/challenge.db')
     conn.execute('pragma foreign_keys=ON') #Turns on foreign key constraints
     cursor = conn.cursor()
@@ -89,9 +161,7 @@ def create_new_url(url, partial_short_url, user_id):
         shortened_url = str(partial_short_url + '/' + str(short_url.encode_url(url_id)))
 
         cursor.execute("""
-            UPDATE URL
-            SET shortUrl=?
-            WHERE id=?
+            UPDATE URL SET shortUrl=? WHERE id=?
             """, (shortened_url, url_id))
 
         conn.commit()
@@ -124,3 +194,24 @@ def create_new_user(user_id):
         conn.rollback()
         conn.close()
         return False
+
+def delete_user(user_id):
+    conn = sqlite3.connect('database/challenge.db')
+    conn.execute('pragma foreign_keys=ON') #Turns on foreign key constraints
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        DELETE FROM User WHERE id=?
+    """, (user_id, ))
+    conn.commit()
+    conn.close()
+
+def delete_url(url_id): #TODO: decrement user url count
+    conn = sqlite3.connect('database/challenge.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        DELETE FROM URLs WHERE id=?
+    """, (url_id, ))
+    conn.commit()
+    conn.close()
--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import db_client
-
-from flask import Flask, request, Blueprint, Response, jsonify
+import json
+from flask import Flask, request, Blueprint, Response, jsonify, redirect
 
 users_bp = Blueprint('users_blueprint', __name__, url_prefix='/users/<string:user_id>')
 stats_bp = Blueprint('stats_blueprint', __name__, url_prefix='/stats')
 app = Flask(__name__)
 
-@app.route('/urls/<int:id>', methods=['GET', 'DELETE'], strict_slashes=False)
-def urls(id):
+@app.route('/urls/<int:url_id>', methods=['GET', 'DELETE'], strict_slashes=False)
+def urls(url_id):
     if request.method == 'GET':
-        return 'get urls'
+        db_response, url = db_client.get_url(url_id)
+
+        if db_response:
+            response = redirect(url, code=301)
+
+        else:
+            response = Response(status=404)
+
+        return response
+
     else:
-        pass
+        db_client.delete_url(url_id)
+        return Response(status=200)
 
 @users_bp.route('/urls', methods=['POST'], strict_slashes=False)
-def urls(user_id):
+def user_urls(user_id):
     json_data = request.get_json(force=True)
     url = json_data['url']
+
+    if url.find("http://") != 0 and url.find("https://") != 0:
+        url = 'http://' + url
+
     partial_short_url = 'http://' + request.host
     db_response, stats = db_client.create_new_url(url, partial_short_url, user_id)
 
@@ -26,6 +40,7 @@ def urls(user_id):
 
     else:
         response = Response(status=409)
+        response.headers['Content-Type'] = 'application/json'
 
     return response
 
@@ -39,7 +54,7 @@ def stats(user_id): #Specific user URL stats
 
     else:
         response = Response(status=404)
-        #response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Type'] = 'application/json'
 
     return response
 
@@ -47,11 +62,12 @@ def stats(user_id): #Specific user URL stats
 @stats_bp.route('/<int:url_id>', methods=['GET'], strict_slashes=False) #Returns stats from a specific URL
 def stats(url_id=None):
     if url_id is None: #global
-        #return 'global stats'
-        return 'Hello, world! running on %s' % request.host
+        response = jsonify(db_client.get_global_stats())
+        response.status_code = 200
+        return response
 
     else: #specific
-        db_response, stats = get_url_stats(url_id)
+        db_response, stats = db_client.get_url_stats(url_id)
 
         if db_response:
             response = jsonify(stats)
@@ -59,8 +75,9 @@ def stats(url_id=None):
 
         else:
             response = Response(status=404)
+            response.headers['Content-Type'] = 'application/json'
 
-        return 'specific stats'
+        return response
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
@@ -73,15 +90,18 @@ def users():
         response = jsonify({'id' : user_id})
         response.status_code = 201
 
-    else: #error: already exists
+    else:
         response = Response(status=409)
-        #response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Type'] = 'application/json'
 
     return response
 
 @app.route('/user/<string:user_id>', methods=['DELETE'], strict_slashes=False) #DELETE /user/:userId was the specification. Maybe they meant to utilize 'users' like in the other URLs?
 def delete(user_id):
-    pass
+    db_client.delete_user(user_id)
+    response = Response(status=200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 #These must be placed here, as the code that runs the blueprints has to be instantiated before registering the blueprint
 app.register_blueprint(users_bp)
